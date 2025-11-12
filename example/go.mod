3cc9a57b5487757e44012e9df566d59076319f7f
lincaiyong <lincaiyong@bytedance.com>
2025-11-12 19:26:18 +0800

    ql

---
+++ example/go.mod
@@ -0,0 +1,12 @@
+module example
+
+go 1.25.1
+
+require github.com/lincaiyong/log v1.0.4
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- example/main.go
+++ example/main.go
@@ -2,17 +2,133 @@ package main
 
 import (
 	"fmt"
-	"github.com/lincaiyong/ql"
+	"github.com/lincaiyong/log"
+	"strconv"
 )
+
+func NewDatabase[T any](data []T) *Database[T] {
+	db := &Database[T]{
+		data:     data,
+		tables:   make([]*Table[T], 0),
+		tableMap: make(map[string]*Table[T]),
+	}
+	table := NewTable[T](db, nil)
+	for i := range data {
+		table.AddRecord(NewRecord[T](table, i, nil))
+	}
+	db.tableMap[""] = table
+	db.tables = append(db.tables, table)
+	return db
+}
+
+type Database[T any] struct {
+	data     []T
+	tables   []*Table[T]
+	tableMap map[string]*Table[T]
+}
+
+func (db *Database[T]) AddTable(baseTableName, tableName string, fields []string, fn func(t T) []string) error {
+	baseTable, ok := db.tableMap[baseTableName]
+	if !ok {
+		return fmt.Errorf("table %s not found", baseTableName)
+	}
+	table := NewTable[T](db, fields)
+	db.tableMap[tableName] = table
+	db.tables = append(db.tables, table)
+	for _, record := range baseTable.records {
+		if ret := fn(db.data[record.this]); ret != nil {
+			r := NewRecord[T](table, record.this, ret)
+			table.AddRecord(r)
+		}
+	}
+	return nil
+}
+
+func (db *Database[T]) Query(q string) []T {
+
+	return nil
+}
+
+func NewTable[T any](db *Database[T], fields []string) *Table[T] {
+	fieldMap := make(map[string]int, len(fields))
+	for i, field := range fields {
+		fieldMap[field] = i
+	}
+	return &Table[T]{
+		db:        db,
+		fields:    fields,
+		fieldMap:  fieldMap,
+		records:   make([]*Record[T], 0),
+		recordMap: make(map[int]*Record[T]),
+	}
+}
+
+type Table[T any] struct {
+	db        *Database[T]
+	fields    []string
+	fieldMap  map[string]int
+	records   []*Record[T]
+	recordMap map[int]*Record[T]
+}
+
+func (t *Table[T]) AddRecord(r *Record[T]) {
+	t.recordMap[r.this] = r
+	t.records = append(t.records, r)
+}
+
+func NewRecord[T any](table *Table[T], this int, data []string) *Record[T] {
+	return &Record[T]{
+		table: table,
+		this:  this,
+		data:  data,
+	}
+}
+
+type Record[T any] struct {
+	table *Table[T]
+	this  int
+	data  []string
+}
 
 func main() {
 	// https://codeql.github.com/docs/codeql-language-guides/analyzing-data-flow-in-go/
-	q := `from Function osOpen, CallExpr call
-where
-  osOpen.hasQualifiedName("os", "Open") and
-  call.getTarget() = osOpen
-select call.getArgument(0)
-`
-	ret := ql.Query(q)
-	fmt.Println(ret)
+	//	q := `from Function osOpen, CallExpr call
+	//where
+	//  osOpen.hasQualifiedName("os", "Open") and
+	//  call.getTarget() = osOpen
+	//select call.getArgument(0)
+	//`
+	//	ret := ql.Query(q)
+	//	fmt.Println(ret)
+	var numbers []string
+	for i := 0; i < 50; i++ {
+		numbers = append(numbers, strconv.Itoa(i))
+	}
+	db := NewDatabase[string](numbers)
+	err := db.AddTable("", "OddNumber", nil, func(t string) []string {
+		if i, _ := strconv.Atoi(t); i%2 == 1 {
+			return []string{}
+		}
+		return nil
+	})
+	if err != nil {
+		log.ErrorLog("fail to add table: %v", err)
+	}
+	err = db.AddTable("", "EvenNumber", []string{"string"}, func(t string) []string {
+		if i, _ := strconv.Atoi(t); i%2 == 0 {
+			return []string{t + "_string"}
+		}
+		return nil
+	})
+	err = db.AddTable("", "DividableBy4", nil, func(t string) []string {
+		if i, _ := strconv.Atoi(t); i%2 == 0 {
+			return []string{}
+		}
+		return nil
+	})
+	ret := db.Query(`from EvenNumber n where n > 6 select n`)
+	for _, n := range ret {
+		log.InfoLog("%d", n)
+	}
+	log.InfoLog("done")
 }
